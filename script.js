e121b6b012b1c3f9da99722470c0cb39a8db59ba
Faustin_King <f.nduwumwamiwabami@gmail.com>
2023-05-09 16:03:44 +0200

    Update querySelector with createElement

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#container');
 
 for (let i = 0; i < 16 * 16; i++) {
-    const square = document.querySelector('div');
-    square.classList.add('square');
-    container.appendChild(square);
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
 }
