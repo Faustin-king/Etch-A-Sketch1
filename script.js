d95c48ffc647a460f7efa63663ab789293af5b56
Faustin_King <f.nduwumwamiwabami@gmail.com>
2023-05-09 16:27:28 +0200

    Change the backgroundColor of divs by using JavaScript through eventListener

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const container = document.querySelector('#container');
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'Coral';
+  });
   container.appendChild(square);
 }
