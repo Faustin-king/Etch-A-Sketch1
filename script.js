e4039ab2424a9a045769dcb0e2c5e88f7df46a83
Faustin_King <f.nduwumwamiwabami@gmail.com>
2023-05-10 07:16:05 +0200

    Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ for (let i = 0; i < 16 * 16; i++) {
   });
   container.appendChild(square);
 }
+
+// select the button element
+const newGridBtn = document.querySelector('#new-grid-btn');
+// Add event listener to the button
+newGridBtn.addEventListener('click', () => {
+  // prompt user for the number of squares per side
+  let gridSize = parseInt(
+    prompt('Enter the number of squares per side (maximum 100):')
+  );
+  // check if gridSize is valid
+  if (!isNaN(gridSize) && gridSize > 0 && gridSize <= 100) {
+    // Remove existing grid
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+
+    // generate new grid
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      sketchpad.appendChild(square);
+    }
+  } else {
+    alert('invalid input!');
+  }
+});
